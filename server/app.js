af5432a1db287d8a396ad7336899615b479c5411
florkaa <florencia.ayc@gmail.com>
2018-07-19 13:44:04 -0300

    proyecto final cors

--- server/app.js
+++ server/app.js
@@ -4,19 +4,18 @@ var cors = require('cors');
 var app = express();
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
- 
-app.use(cors())
-
-var indexRouter = require('./routes/index');
 
 var app = express();
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', require('./routes/index'));
+
+app.options('*', cors())
 
 module.exports = app;
